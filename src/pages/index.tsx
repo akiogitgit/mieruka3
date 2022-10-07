97d2e750264c688e1931995e81dc4e26e94525ab
akiogitgit <akimaron2828@icloud.com>
2022-10-07 17:27:48 +0900

    feat: supabaseと接続

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,59 @@
-import { Button } from "@mantine/core"
+import { Button, Center } from "@mantine/core"
 import type { NextPage } from "next"
 import { Layout } from "../components/Layout"
 import Link from "next/link"
+import { Sample } from "../types/sample"
 
 const Home: NextPage = () => {
+  const samples: Sample[] = [
+    {
+      id: 1,
+      text: "string",
+      label: "good",
+      published: true,
+      posts: [
+        {
+          id: 1,
+          content: "content",
+        },
+      ],
+    },
+    {
+      id: 2,
+      text: "string2",
+      label: "bad",
+      published: false,
+      posts: [
+        {
+          id: 1,
+          content: "content",
+        },
+      ],
+    },
+  ]
+
   return (
     <Layout>
-      <div className='flex gap-3'>
-        <button className='font-bold bg-blue-500 text-white py-2.3 px-3'>
-          button
-        </button>
-        <Button>button</Button>
-        <Link href='/sample'>sampleページに移動</Link>
+      <div>
+        <div className='flex gap-3'>
+          <button className='font-bold bg-blue-500 text-white py-2.3 px-3'>
+            button
+          </button>
+          <Button>button</Button>
+        </div>
+        <ul className='flex flex-wrap gap-10'>
+          {samples?.map(sample => (
+            <li key={sample.id} className='w-200px'>
+              <p>{sample.id}</p>
+              <p>{sample.text}</p>
+              <p>{sample.label}</p>
+              <p>{sample.published ? "true" : "false"}</p>
+            </li>
+          ))}
+        </ul>
+        <Center>
+          <Link href='/sample'>sampleページに移動</Link>
+        </Center>
       </div>
     </Layout>
   )
