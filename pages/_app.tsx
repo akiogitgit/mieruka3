7864ea8f8fa6b6ebc53d12a050c8bd6913dcb448
akiogitgit <akimaron2828@icloud.com>
2022-10-07 16:49:18 +0900

    feat: mantine 導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'windi.css'
+import type { AppProps } from "next/app"
+import "windi.css"
+import { MantineProvider } from "@mantine/core"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        // colorScheme: "dark",
+        fontFamily: "Verdana, sans-serif",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
 
 export default MyApp
