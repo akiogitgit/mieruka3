9b21620e76e883c490333f6c92bba5a70f877070
yusuke tsushima <tuple@yusukenoMacBook-Pro.local>
2022-10-09 15:28:52 +0900

    feat: fetch smoked data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,53 +3,68 @@ import type { NextPage } from "next"
 import { Layout } from "../components/Layout"
 import Link from "next/link"
 import { Sample } from "../types/sample"
+import { useCallback, useEffect, useState } from "react"
+import { Session } from "@supabase/supabase-js"
+import { supabase } from "../utils/supabase"
+import { useGetApi } from "../hooks/useGetApi"
+import { useIsLoggedIn } from "../hooks/useIsLoggedIn"
 
 // メイン画面（継続日数、節約金額、応援メッセージ、リスクが下がった）
 // 肺の背景が少し明るくするとか
 // 臓器くん
 
 const Home: NextPage = () => {
-  const samples: Sample[] = [
-    {
-      id: 1,
-      text: "string",
-      label: "good",
-      published: true,
-      posts: [
-        {
-          id: 1,
-          content: "content",
-        },
-      ],
-    },
-    {
-      id: 2,
-      text: "string2",
-      label: "bad",
-      published: false,
-      posts: [
-        {
-          id: 1,
-          content: "content",
-        },
-      ],
-    },
-  ]
+  const session = useIsLoggedIn()
+
+  const [duration, setDuration] = useState(0)
+  const [savingPrice, setSavingPrice] = useState(0)
+  const [lifespan, setLifespan] = useState(0)
+
+  console.log("sesion", session)
+
+  // ログインしていたら、継続禁煙時間などをセット
+  const setRecordings = useCallback(async () => {
+    const userId = session?.user?.id
+    console.log("userId", userId)
+    if (userId !== undefined && userId !== null) {
+      const { data, error } = await supabase
+        .from("smoked")
+        .select("*")
+        .eq("user_id", userId)
+        .order("created_at", { ascending: false })
+        .maybeSingle()
+
+      if (error) {
+        console.error("data", data)
+        throw new Error(error.message)
+      }
+      console.log("data", data)
+    }
+  }, [session])
+
+  useEffect(() => {
+    setRecordings()
+  }, [setRecordings])
 
   return (
     <Layout>
       <div>
-        <Stack className='flex flex-wrap gap-10'>
+        <Stack className='flex flex-wrap mb-6 gap-10'>
           <div>
             継続日数
-            <div></div>
+            <div>{duration}</div>
           </div>
-          <div>節約金額</div>
-          <div>寿命伸びた</div>
-          <div>応援メッセージ</div>
-          <div>リスクが下がった</div>
           <div>
-            臓器くん
+            節約金額
+            <div>{savingPrice}</div>
+          </div>
+          <div>
+            伸びた寿命
+            <div>{lifespan}</div>
+          </div>
+          {/* <div>応援メッセージ</div>
+          <div>リスクが下がった</div> */}
+          <Center>
             <Group spacing='xs'>
               <div>
                 <div
@@ -74,7 +89,7 @@ const Home: NextPage = () => {
                 <p>寿命が-1000日伸びたよ！</p>
               </div>
             </Group>
-          </div>
+          </Center>
         </Stack>
         <Center>
           <Stack>
