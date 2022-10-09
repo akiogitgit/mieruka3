c9cf1075e4a61f599b676e27d74c163f04ba7f7e
KotaYoshikawa <clown.66137@gmail.com>
2022-10-09 13:18:27 +0900

    add calendarGraph

--- src/hooks/useGetApi.ts
+++ src/hooks/useGetApi.ts
@@ -5,23 +5,24 @@ import { supabase } from "../utils/supabase"
 type Props = {
   order?: string
   ascending?: boolean
+  select?: string
 }
 
 export const useGetApi = <Data = any>(
   tableName: string,
-  { order = "created_at", ascending = true }: Props,
+  { order = "created_at", ascending = true, select = "*" }: Props,
 ) => {
   const getData = useCallback(async () => {
     const { data, error } = await supabase
       .from(tableName)
-      .select("*")
+      .select(select)
       .order(order, { ascending })
 
     if (error) {
       throw new Error(error.message)
     }
     return data
-  }, [ascending, order, tableName])
+  }, [ascending, order, select, tableName])
 
   return useQuery<Data[]>({
     queryKey: [tableName],
--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,9 +1,47 @@
 import React from "react"
 import { Layout } from "../components/Layout"
+import { useEffect, useState } from "react"
+import { Calendar } from "@mantine/dates"
+import { NextPage } from "next"
+import { useGetApi } from "../hooks/useGetApi"
 
 // カレンダー（吸った日、禁断症状出た日）
-const Calendar = () => {
-  return <Layout>Calendar</Layout>
+const CalendarGraph: NextPage = () => {
+  const { data } = useGetApi("profiles", { select: "created_at" })
+  //const data = query.data
+
+  console.log(data)
+
+  const now = new Date()
+  //const date = new Date('2022-10-09T02:38:33.259262+00:00')
+  const [value, setValue] = useState<Date[]>()
+
+  const datelist =
+    data?.map(item => {
+      const date = new Date(item?.created_at)
+      // return new Date(
+      //   `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+      // )
+      return date
+    }) ?? (() => [new Date()])
+
+  useEffect(() => {
+    // 最新のSessionで更新
+
+    setValue(datelist)
+  }, [])
+
+  console.log("value:", value)
+  console.log("datelist:", datelist)
+
+  return (
+    <Layout>
+      <div>
+        <h1>カレンダー</h1>
+        <Calendar multiple value={value} onChange={setValue} size='md' />
+      </div>
+    </Layout>
+  )
 }
 
-export default Calendar
+export default CalendarGraph
