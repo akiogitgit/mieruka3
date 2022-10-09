9e7e331ef9afc4b73a6a8540b503e27c618564bf
akiogitgit <akimaron2828@icloud.com>
2022-10-09 14:40:37 +0900

    feat: ScrollArea追加

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,4 +1,12 @@
-import { Avatar, Badge, Button, Indicator, Paper, Modal } from "@mantine/core"
+import {
+  Avatar,
+  Badge,
+  Button,
+  Indicator,
+  Paper,
+  Modal,
+  ScrollArea,
+} from "@mantine/core"
 import React, { useCallback, useEffect, useState } from "react"
 import { ChatCreateButton } from "../components/ChatCreateButton"
 import { ChatItemGoodButton } from "../components/ChatItemGoodButton"
@@ -44,38 +52,40 @@ const Chat = () => {
         <ChatCreateButton />
       </div>
 
-      <div className='flex flex-col mt-4 gap-6'>
-        {chats?.map(chat => (
-          <div key={chat.id} className='flex gap-3 items-start'>
-            <Indicator
-              inline
-              label=''
-              size={16}
-              offset={7} // 内側に7
-              color='green'
-              position='bottom-end'
-              withBorder // 外側の白
-              processing // 主張激しくなる
-            >
-              <Avatar
-                size='lg'
-                radius='xl'
-                className='transform duration-300 hover:scale-105'
-                src='https://source.unsplash.com/random'
-              />
-            </Indicator>
-            <div>
-              <p>{chat.user_name}</p>
-              <Paper shadow='sm' p='sm' className='min-w-200px max-w-400px'>
-                <div className='whitespace-pre-wrap'>{chat.message}</div>
-                <div className='mt-2'>
-                  <ChatItemGoodButton chat={chat} />
-                </div>
-              </Paper>
+      <ScrollArea scrollHideDelay={300} className='h-[calc(100vh-210px)] mt-3'>
+        <div className='flex flex-col mt-4 gap-6'>
+          {chats?.map(chat => (
+            <div key={chat.id} className='flex gap-3 items-start'>
+              <Indicator
+                inline
+                label=''
+                size={16}
+                offset={7} // 内側に7
+                color='green'
+                position='bottom-end'
+                withBorder // 外側の白
+                processing // 主張激しくなる
+              >
+                <Avatar
+                  size='lg'
+                  radius='xl'
+                  className='transform duration-300 hover:scale-105'
+                  src='https://source.unsplash.com/random'
+                />
+              </Indicator>
+              <div>
+                <p>{chat.user_name}</p>
+                <Paper shadow='sm' p='sm' className='min-w-200px max-w-400px'>
+                  <div className='whitespace-pre-wrap'>{chat.message}</div>
+                  <div className='mt-2'>
+                    <ChatItemGoodButton chat={chat} />
+                  </div>
+                </Paper>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </ScrollArea>
     </Layout>
   )
 }
