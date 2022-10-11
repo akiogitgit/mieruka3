69c15e5567b8a7641419321f0b9cb43416906cf8
akiogitgit <akimaron2828@icloud.com>
2022-10-11 16:35:51 +0900

    feat: direct-message.tsxにグローバルステートを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Layout } from "../components/Layout"
 import { ProfileDetail } from "../components/ProfileDetail"
 import SmokedButton from "../components/SmokedButton"
 import useStore from "../store"
-import { useGetSession } from "../hooks/useGetSession"
 
 // メイン画面（継続日数、節約金額、応援メッセージ、リスクが下がった）
 // 肺の背景が少し明るくするとか
