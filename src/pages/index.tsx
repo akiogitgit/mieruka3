293906a0be19e0d97c5fb357a054609c03302bba
yusuke tsushima <tuple@yusukenoMacBook-Pro.local>
2022-10-12 00:30:06 +0900

    feat: add signage list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Center, Stack, Text } from "@mantine/core"
 import type { NextPage } from "next"
 import { Layout } from "../components/Layout"
 import { ProfileDetail } from "../components/ProfileDetail"
+import { SignageList } from "../components/SignageList"
 import SmokedButton from "../components/SmokedButton"
 import { SmokedChart } from "../components/SmokedChart"
 import useStore from "../store"
@@ -30,6 +31,7 @@ const Home: NextPage = () => {
           </div>
         )}
       </div>
+      <SignageList />
     </Layout>
   )
 }
