306974d80342d1f38f773b84a5189f8ffdc475f8
yusuke tsushima <tuple@yusukenoMacBook-Pro.local>
2022-10-11 12:02:10 +0900

    feat: add chart component in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Layout } from "../components/Layout"
 import { ProfileDetail } from "../components/ProfileDetail"
 import SmokedButton from "../components/SmokedButton"
 import { useIsLoggedIn } from "../hooks/useIsLoggedIn"
+import { Chart } from "../components/chart"
 
 // メイン画面（継続日数、節約金額、応援メッセージ、リスクが下がった）
 // 肺の背景が少し明るくするとか
@@ -19,9 +20,12 @@ const Home: NextPage = () => {
         <Stack className='mb-6'>
           <ProfileDetail />
         </Stack>
-        <Center>
-          <SmokedButton />
-        </Center>
+        <Stack>
+          <Center>
+            <SmokedButton />
+          </Center>
+          <Chart />
+        </Stack>
       </div>
     </Layout>
   )
